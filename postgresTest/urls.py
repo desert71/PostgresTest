9ff830d16c2961a54501de1780513e9cafb90463
desert71 <amkazennov99@gmail.com>
2023-11-27 16:11:05 +0300

    Update views.py & apps.py

--- postgresTest/urls.py
+++ postgresTest/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from testdb.views import goal_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('goals/', goal_list, name='goal_list')
 ]
--- testdb/views.py
+++ testdb/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Goal
 
-# Create your views here.
+def goal_list(request):
+    goals = Goal.objects.goals()
+    data = {"goals": list(goals.values())}
+    return JsonResponse(data)
+
