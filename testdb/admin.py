502ec36486c08f589d383701d0234638a3a4767d
desert71 <amkazennov99@gmail.com>
2023-11-27 10:55:41 +0300

    Update Admin.py

--- testdb/admin.py
+++ testdb/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Goal
 
-# Register your models here.
+@admin.register(Goal)
+class GoalAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'goal_type', 'start_date', 'finish_date',)
+    list_filter = ('goal_type', 'start_date', 'finish_date',)
+    search_fields = ('name',)
+    ordering = ('-start_date',)
