cc8228d7207f6d8bc2506e945c8e638899b2320f
desert71 <amkazennov99@gmail.com>
2023-11-29 11:17:15 +0300

    Upgrade settings.py, .env.axample

--- postgresTest/settings.py
+++ postgresTest/settings.py
@@ -86,11 +86,11 @@ WSGI_APPLICATION = 'postgresTest.wsgi.application'
 DATABASES = {
    'default': {
        'ENGINE': 'django.db.backends.postgresql',
-       'NAME': 'test',
-       'USER': 'postgres',
-       'PASSWORD': 'password',
-       'HOST': 'localhost',
-       'PORT': '5432',
+       'NAME': config('DB_NAME'),
+       'USER': config('DB_USER'),
+       'PASSWORD': config('DB_PASSWORD'),
+       'HOST': config('DB_HOST'),
+       'PORT': config('DB_PORT'),
    }
 }
 
